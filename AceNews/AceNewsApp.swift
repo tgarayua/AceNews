048c41e730e2ddef4ccf9b1e97071b664cc87023
Thomas Garayua <tgarayua@gmail.com>
2023-02-07 04:42:30 -0500

    Initial Commit

---
+++ AceNews/AceNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AceNewsApp.swift
+//  AceNews
+//
+//  Created by Thomas G on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct AceNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
