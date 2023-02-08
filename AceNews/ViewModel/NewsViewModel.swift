20d4306b4a9ed30acc96f2f8ae4ac6c4e99b9db2
Thomas Garayua <tgarayua@gmail.com>
2023-02-08 00:45:09 -0500

    NewsViewModel created

---
+++ AceNews/ViewModel/NewsViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  NewsViewModel.swift
+//  AceNews
+//
+//  Created by Thomas G on 2/8/23.
+//
+
+import Foundation
+import Combine
+
+protocol NewsViewModel {
+    func getArticles()
+}
+
+class NewsViewModelImpl: ObservableObject, NewsViewModel {
+    
+    private let service: NewsService
+    
+    private(set) var articles = [Article]()
+    private var cancellables = Set<AnyCancellable>()
+    
+    @Published private(set) var state: ResultState = .loading
+    
+    init(service: NewsService) {
+        self.service = service
+    }
+    
+    func getArticles() {
+        
+        self.state = .loading
+        
+        let cancellable = service
+            .request(from: .getNews)
+            .sink { res in
+                switch res {
+                    
+                case .finished:
+                    self.state = .success(content: self.articles)
+                    // Send back the articles
+                    break
+                case .failure(let error):
+                    self.state = .failed(error: error)
+                    // Send back the error
+                    break
+                }
+            } receiveValue: { response in
+                self.articles = response.articles
+            }
+        
+        self.cancellables.insert(cancellable)
+    }
+}
